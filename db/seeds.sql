b8dee803e086a6e0360a9ec08d145f5ee09d2c3d
Kyle Pace <kylelpace@gmail.com>
2020-10-27 22:28:56 -0600

    coonecting to heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Double Double Animal Style", false), ("Shackburger", false), ("Crown Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Double Animal Style", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Shackburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Crown Burger", FALSE);
